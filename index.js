b8ae69e2f258a5299526aeae1a70b83f2a04191f
SuperJav <javielito2405@hotmail.com>
2023-05-23 14:23:43 -0400

    areglando falla #3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express =require("express")
 const app= express()
 const cours= require("cors")
 
-app.use(express.static('./build'))
+app.use(express.static('build'))
 app.use(express.json())
 app.use(cours())
 
